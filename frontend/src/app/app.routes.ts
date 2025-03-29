6c91769a1fd91d54fbfa61ce0d94ecf4a96fa0fb
Nuridin <nurmannuridin28@gmail.com>
2025-03-29 19:40:56 +0500

    initial registration page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
-import {LoginComponent} from './features/auth/login/login.component';
-import {DashboardComponent} from './pages/dashboard/dashboard.component';
-import {NotFoundComponent} from './pages/not-found/not-found.component';
+import { LoginComponent } from './features/auth/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { RegisterComponent } from './features/auth/register/register.component';
 
 export const routes: Routes = [
-  {path: '', component: LoginComponent},
-  {path: 'dashboard', component: DashboardComponent},
-  {path: '**', component: NotFoundComponent},
+  { path: 'auth/login', component: LoginComponent },
+  { path: 'auth/register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '**', component: NotFoundComponent },
 ];
