65c75ed51dd558d469fa10fa7cbc58890d1f9bbd
Nuridin <nurmannuridin28@gmail.com>
2025-03-29 19:34:37 +0500

    initial login page

--- frontend/src/app/features/auth/login/login.component.ts
+++ frontend/src/app/features/auth/login/login.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
+  imports: [FormsModule],
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
+  hidePassword: boolean = true;
 
+  togglePasswordVisibility() {
+    this.hidePassword = !this.hidePassword;
+  }
+
+  onSubmit() {
+    console.log('Login Data:', {
+      username: this.username,
+      password: this.password,
+    });
+  }
 }
