39dc51d347d12a895e73744fad66356d4d5c2968
Sultan <sultanbaibolov1@gmail.com>
2025-04-06 15:58:17 +0500

    categories 50%

--- frontend/src/app/layouts/sidebar/sidebar.component.ts
+++ frontend/src/app/layouts/sidebar/sidebar.component.ts
@@ -1,14 +1,41 @@
-import { Component } from '@angular/core';
-import {FileExplorerComponent} from '../../features/documents/file-explorer/file-explorer.component';
+import { Component, signal, computed } from '@angular/core';
+import { FileExplorerComponent } from '../../features/documents/file-explorer/file-explorer.component';
+import { CommonModule } from '@angular/common';
+import {FileNode} from '../../core/file-node';
 
 @Component({
   selector: 'app-sidebar',
   imports: [
-    FileExplorerComponent
+    FileExplorerComponent,
+    CommonModule,
   ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+
+  toggleFolder(node: FileNode) {
+    node.expanded = !node.expanded;
+  }
+
+  treeData: FileNode[] = [
+    {
+      name: 'Projects',
+      type: 'folder',
+      expanded: false,
+      children: [
+        { name: 'Project1.docx', type: 'document' },
+        {
+          name: 'Archive',
+          type: 'folder',
+          expanded: false,
+          children: [
+            { name: 'OldProject.docx', type: 'document' }
+          ]
+        }
+      ]
+    },
+    { name: 'Readme.md', type: 'document' }
+  ];
 
 }
